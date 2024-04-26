57ef52b0fc853ec424dd2f794854e1319aca146e
Ivarmmme <162916474+Ivarmmme@users.noreply.github.com>
2024-04-27 02:19:38 +0530

    Update clubbot.py

--- clubbot.py
+++ clubbot.py
@@ -4,7 +4,7 @@ from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.constants import ParseMode
 from telegram.ext import Application, CommandHandler, ContextTypes, CallbackQueryHandler
 from database import load_data, save_data   
-import uuid
+
 
 # Function to handle the /req command
 async def request_to_join(update: Update, context: ContextTypes.DEFAULT_TYPE):
